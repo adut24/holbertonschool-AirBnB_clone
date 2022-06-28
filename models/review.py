72f649864e3e0fe86c741d1d968fb826f480b0af
Pierre Dureau <dureaupierre53@gmail.com>
2022-06-28 14:28:21 +0200

    Add Place & Review class

---
+++ models/review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module Review
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Class Review"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
